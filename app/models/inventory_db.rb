23a23238464e797cd1ac1af6dec63f31df9d1840
Sue Grimes <sgrimes@stanford.edu>
2012-09-19 12:44:11 -0700

    Modification to staging db; remove obsolete mysql.rb

--- app/models/inventory_db.rb
+++ app/models/inventory_db.rb
@@ -1,4 +1,4 @@
 class InventoryDB < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection (:inventory) if (RAILS_ENV == 'production' || RAILS_ENV == 'staging')
+  establish_connection (:inventory) if RAILS_ENV == 'production' 
 end
