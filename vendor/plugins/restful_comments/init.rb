acad2f1d1cfe76806301486fdfbc40927df82cd1
Jennifer Palm <jpalm@stanford.edu>
2013-11-06 13:34:22 -0800

    rem'ing files from vendor/plugins and public/stylesheets

--- vendor/plugins/restful_comments/init.rb
+++
@@ -1,5 +0,0 @@
-require 'restful_comments_commentable'
-require 'restful_comments_helper'
-
-ActiveRecord::Base.send :include, RestfulComments::Commentable
-ActionView::Base.send :include, RestfulComments::Helper
